dbb7d5b931b5b05c93a34600c36fb95a8eb2dedc
lemin2601 <lemin2601@gmail.com>
2019-05-01 09:53:43 +0700

    .

--- packages/hot-update/main.js
+++ packages/hot-update/main.js
@@ -2,52 +2,36 @@
 var path = require('path');
 var fs = require('fs');
 
-function onBeforeBuildFinish(options, callback){
-    //options.platform
-    //options.dest
-    Editor.log('Building ' + options.platform + ' to ' + options.dest); // you can display a log in the Console panel
-    var mainJsPath = path.join(options.dest, 'main.js');  // get path of main.js in build folder
-    var script = fs.readFileSync(mainJsPath, 'utf8');     // read main.js
-    script += '\n' + 'window.myID = "01234567";';         // append any scripts as you need
-    fs.writeFileSync(mainJsPath, script);                 // save main.js
-    callback();
-}
-
 function onBuildStart(options, callback){
-    Editor.log("On Build Start");
-
-    callback();
-}
-
-function onBeforeChangeFiles(option,callback){
-    Editor.log("On Before Change Files");
-
+    Editor.log('Hot-update onBuildStart');
     Editor.log('Building ' + options.platform + ' to ' + options.dest); // you can display a log in the Console panel
-
-    callback();
 }
 
-function onBuildFinished(option,callback){
-    Editor.log("on Before Build Finished");
-
-    callback();
+function onBeforeChangeFiles(options, callback){
+    Editor.log('Hot-update onBeforeChangeFiles');
+    Editor.log('Building ' + options.platform + ' to ' + options.dest); // you can display a log in the Console panel
 }
+
+
+function onBuildFinished(options, callback){
+    Editor.log('Hot-update onBuildFinished');
+    Editor.log('Building ' + options.platform + ' to ' + options.dest); // you can display a log in the Console panel
+}
+
 
 
 module.exports = {
     load(){
+        // execute when package loaded
         Editor.Builder.on('build-start', onBuildStart);
         Editor.Builder.on('before-change-files', onBeforeChangeFiles);
         Editor.Builder.on('build-finished', onBuildFinished);
-
-        Editor.Builder.on('before-change-files', onBeforeBuildFinish);
     },
     unload(){
+        // execute when package unloaded
         Editor.Builder.removeListener('build-start', onBuildStart);
         Editor.Builder.removeListener('before-change-files', onBeforeChangeFiles);
         Editor.Builder.removeListener('build-finished', onBuildFinished);
-
-        Editor.Builder.removeListener('before-change-files', onBeforeBuildFinish);
     },
     // register your ipc messages here
     messages: {
