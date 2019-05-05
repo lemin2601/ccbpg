e2a4c5cb37cc8e4d2adb96876c6b0a02927594cb
lemin2601 <lemin2601@gmail.com>
2019-05-05 13:46:23 +0700

    .

---
+++ build/jsb-link/src/settings.js
@@ -0,0 +1,45 @@
+window._CCSettings = {
+    platform: "android",
+    groupList: [
+        "default"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ]
+    ],
+    rawAssets: {
+        assets: {
+            "de8/Ea8xxN7K06uICQhiws": [
+                "configs/config.json",
+                "cc.JsonAsset"
+            ]
+        }
+    },
+    launchScene: "db://assets/scenes/SceneLoading.fire",
+    scenes: [
+        {
+            url: "db://assets/scenes/SceneLoading.fire",
+            uuid: "982y9+lF1EV4AtybbSAa0W"
+        },
+        {
+            url: "db://assets/scenes/SceneMenu.fire",
+            uuid: "f7tLo9CC9IX49phdMc84sG"
+        }
+    ],
+    packedAssets: {
+        "0631a0b23": [
+            "99FwsL0hBG8bITfZ4/IwmK",
+            "cf73jxyN9Jt47QTJU6ziYh"
+        ],
+        "066ad646f": [
+            "675ovJ2tVK2aLYfgPUWOMv",
+            "8855/VlrRKd6H0MSRnFxAU",
+            "982y9+lF1EV4AtybbSAa0W"
+        ]
+    },
+    md5AssetsMap: {},
+    orientation: "",
+    debug: true,
+    subpackages: {}
+};
